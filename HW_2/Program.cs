d35572087afbb607d8b43f5f22284f14bc3556f9
insomnea42 <khveroe@gmail.com>
2022-08-16 15:07:13 +0500

    All HW for second seminar

---
+++ HW_2/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.Write("Введите число: ");
+
+int chislo = Convert.ToInt32(Console.ReadLine());
+int res = 0, n = 999, i = 10, del = 1;
+
+if (chislo > 99)
+{
+    while (res == 0)
+    {
+        if (chislo <= n)
+        {
+            res = chislo % i;
+            res = res / del;
+            Console.WriteLine("Третья цифра числа: " + res);
+            break;
+        }
+        else
+        {
+            n = n * 10 + 9;
+            i = i * 10;
+            del = del * 10;
+        }
+
+    }
+}
+else
+{
+    Console.WriteLine("Третья цифра отсутствует");
+}
